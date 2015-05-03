cf0161385a142b55aad693b9b1fde9045887fb88
j-ew-s <gabriel.scavassa@outlook.com>
2015-05-03 12:47:45 -0300

    Cópia do Estudo Rest

---
+++ SlimProdutos/index.php
@@ -0,0 +1,58 @@
+<?php
+
+require '../vendor/Slim/Slim.php';
+\Slim\Slim::registerAutoloader();
+$app = new \Slim\Slim();
+$app->response()->header('Content-Type', 'application/json;charset=utf-8');
+
+// DATA BASE CONNECTION
+function getConn()
+{
+	return new PDO('mysql:host=localhost;dbname=EntregaRapida',
+	'root',
+	'aesgep13',
+	array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
+	);
+
+};
+
+//ROUTES
+//default route
+$app->get('/', function () {
+	echo "SlimProdutos ";
+});
+// calling function
+$app->get('/categorias','getCategorias');
+
+//GET REQUEST WITH PARAMETERS
+$app->get('/cat', function () use ($app) {
+	//RECIEVE PARAMETERS FROM GET
+   	$nomeUsuario= $app->request()->get('nome');
+   	$senha = $app->request()->get('senha');
+	try{
+	   	//START A CONNECTION AND EXECUTE SQL VIA PDO
+	   	$conn = getConn();
+		$sql = "SELECT * FROM Acessos WHERE nomeUsuario = :nomeUsuario AND senha = :senha";
+		$stmt = $conn->prepare($sql);
+		$stmt->bindParam("nomeUsuario",$nomeUsuario);
+		$stmt->bindParam("senha",$senha);
+		$stmt->execute();
+		$acesso = $stmt->fetchObject();
+		//RESPONSE SUCCESS
+		echo $_GET['callback'] . "({result:".json_encode($acesso)."})";
+	}
+	catch (Exception $e) {
+    	//RESPONSE ERROR
+		echo $_GET['callback'] . "({result:".json_encode($e->getMessage())."})";
+	}
+});
+
+function getCategorias()
+{
+	$stmt = getConn()->query("SELECT * FROM Categorias");
+	$categorias = $stmt->fetchAll(PDO::FETCH_OBJ);
+	echo "{categorias:".json_encode($categorias)."}";
+};
+
+
+$app->run();
